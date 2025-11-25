0c461b658092e72780cc73e832c10e79cacd72ea
Haroon Zia <haroonzia9301@gmail.com>
2025-11-25 20:11:06 +0500

    Fix app name and icon

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.supersix.mbbsprep',
-  appName: 'SuperSix MBBS Prep',
+  appName: 'ProffMaster',
   webDir: 'dist'
 };
 
